5e29d501c89415ad989a163c5f020e142e11df6a
Thaohocdot <122032676+trungkongu2@users.noreply.github.com>
2023-01-10 11:30:49 +0700

    BE

---
+++ NHOM34/src/main/java/com/example/nhom34/Nhom34Application.java
@@ -0,0 +1,13 @@
+package com.example.nhom34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom34Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nhom34Application.class, args);
+    }
+
+}
---
+++ NHOM34/src/test/java/com/example/nhom34/Nhom34ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nhom34;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom34ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
